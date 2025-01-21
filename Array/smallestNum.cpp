34eb066af20b981fea785235f264b42b8c13a7f6
rafia23 <rafiaferdousduti@gmail.com>
2025-01-21 08:41:27 +0600

    added index

--- Array/smallestNum.cpp
+++ Array/smallestNum.cpp
@@ -9,13 +9,26 @@ int main(){
     int smallest = INT_MAX;
     int largest = INT_MIN;
 
+    int  smallestIndex = -1;
+    int largestIndex = -1;
+
+    // for(int i=0; i<size; i++){
+    //     smallest = min(nums[i],smallest);
+    //     largest = max(nums[i],largest);
+    // }
+
     for(int i=0; i<size; i++){
-        smallest = min(nums[i],smallest);
-        largest = max(nums[i],largest);
+        if(nums[i]<smallest){
+            smallest = nums[i];
+            smallestIndex = i;
+        }  if(nums[i] > largest){
+            largest = nums[i];
+            largestIndex = i; 
+        }
     }
 
-    cout<< "smallest: "<<smallest<<endl;
-    cout<< "largest: "<<largest<<endl;
+    cout<< "smallest number "<<smallest<<" at index: "<<smallestIndex<<endl;
+     cout<< "largest number "<<largest<<" at index: "<<largestIndex<<endl;
 
     return 0;
 }
